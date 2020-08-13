846384da1f7846f7c7b99c947db52b9bb3cbbbad
Oleg <sorokin.world@gmail.com>
2020-08-13 22:47:45 +0300

    [dev] List. Added sample

--- public/temp/personal.js
+++ public/temp/personal.js
@@ -6,6 +6,8 @@ var data = [
 		phone: "(405) 555-0128",
 		mail: "kmccoy@gmail.com",
 		photo: "../temp/img/big-avatar-1.jpg",
+		birthday: "10.01.1956",
+		start:"15.09.1990"
 	},
 	{
 		id: "1",
@@ -14,6 +16,8 @@ var data = [
 		phone: "(405) 632-1372",
 		mail: "tfisher@gmail.com",
 		photo: "../temp/img/big-avatar-2.jpg",
+		birthday: "09.12.1987",
+		start:"04.02.2018"
 	},
 	{
 		id: "1.1",
@@ -22,6 +26,8 @@ var data = [
 		phone: "(402) 371-6736",
 		mail: "fsaunders@gmail.com",
 		photo: "../temp/img/big-avatar-3.jpg",
+		birthday: "25.05.1997",
+		start:"12.09.2019"
 	},
 	{
 		id: "1.1.1",
@@ -30,6 +36,8 @@ var data = [
 		phone: "(346) 622-8633",
 		mail: "jbrown@gmail.com",
 		photo: "../temp/img/big-avatar-14.jpg",
+		birthday: "03.10.1970",
+		start:"06.02.1998"
 	},
 	{
 		id: "1.1.1.1",
@@ -38,6 +46,8 @@ var data = [
 		phone: "(846) 962-1723",
 		mail: "rmarshall@gmail.com",
 		photo: "../temp/img/big-avatar-16.jpg",
+		birthday: "02.11.1984",
+		start:"12.06.2015"
 	},
 	{
 		id: "1.1.1.2",
@@ -46,6 +56,8 @@ var data = [
 		phone: "(763) 213-8373",
 		mail: "twalsh@gmail.com",
 		photo: "../temp/img/big-avatar-17.jpg",
+		birthday: "15.03.1978",
+		start:"15.08.1999"
 	},
 	{
 		id: "1.1.1.3",
@@ -54,6 +66,8 @@ var data = [
 		phone: "(364) 234-7523",
 		mail: "hpearce@gmail.com",
 		photo: "../temp/img/big-avatar-18.jpg",
+		birthday: "12.12.1990",
+		start:"05.03.2017"
 	},
 	{
 		id: "1.1.2",
@@ -62,6 +76,9 @@ var data = [
 		phone: "(578) 342-1237",
 		mail: "abarnes@gmail.com",
 		photo: "../temp/img/big-avatar-19.jpg",
+		dir: "vertical",
+		birthday: "15.03.1954",
+		start:"09.12.1986"
 	},
 	{
 		id: "1.1.2.1",
@@ -70,6 +87,8 @@ var data = [
 		phone: "(832) 426-2223",
 		mail: "egreen@gmail.com",
 		photo: "../temp/img/big-avatar-20.jpg",
+		birthday: "01.06.1957",
+		start:"12.02.1976"
 	},
 	{
 		id: "1.1.2.2",
@@ -78,6 +97,8 @@ var data = [
 		phone: "(523) 125-2523",
 		mail: "dbarrett@gmail.com",
 		photo: "../temp/img/big-avatar-21.jpg",
+		birthday: "09.09.1948",
+		start:"04.03.1968"
 	},
 	{
 		id: "1.1.3",
@@ -86,6 +107,8 @@ var data = [
 		phone: "(251) 315-4731",
 		mail: "ajohnston@gmail.com",
 		photo: "../temp/img/big-avatar-22.jpg",
+		birthday: "01.07.1956",
+		start:"09.01.2020"
 	},
 	{
 		id: "1.1.3.1",
@@ -94,6 +117,8 @@ var data = [
 		phone: "(151) 231-1256",
 		mail: "pholmes@gmail.com",
 		photo: "../temp/img/big-avatar-23.jpg",
+		birthday: "07.01.2000",
+		start:"25.12.2019"
 	},
 	{
 		id: "1.1.3.2",
@@ -102,6 +127,8 @@ var data = [
 		phone: "(125) 215-2636",
 		mail: "cfraser@gmail.com",
 		photo: "../temp/img/big-avatar-24.jpg",
+		birthday: "05.08.1988",
+		start:"07.06.2010"
 	},
 	{
 		id: "2",
@@ -110,6 +137,8 @@ var data = [
 		phone: "(405) 372-9756",
 		mail: "ahall@gmail.com",
 		photo: "../temp/img/big-avatar-4.jpg",
+		birthday: "04.04.1983",
+		start:"05.03.2015"
 	},
 	{
 		id: "2.1",
@@ -118,6 +147,9 @@ var data = [
 		phone: "(124) 622-1256",
 		mail: "mhunter@gmail.com",
 		photo: "../temp/img/big-avatar-25.jpg",
+		dir: "vertical",
+		birthday: "04.03.1989",
+		start:"09.01.2020"
 	},
 	{
 		id: "2.1.1",
@@ -126,6 +158,8 @@ var data = [
 		phone: "(325) 154-6232",
 		mail: "bsutton@gmail.com",
 		photo: "../temp/img/big-avatar-26.jpg",
+		birthday: "01.12.1993",
+		start:"11.05.2019"
 	},
 	{
 		id: "2.1.2",
@@ -134,6 +168,8 @@ var data = [
 		phone: "(165) 463-1232",
 		mail: "jstevens@gmail.com",
 		photo: "../temp/img/big-avatar-27.jpg",
+		birthday: "25.11.1986",
+		start:"07.03.2005"
 	},
 	{
 		id: "2.1.3",
@@ -142,6 +178,8 @@ var data = [
 		phone: "(578) 734-3633",
 		mail: "akhan@gmail.com",
 		photo: "../temp/img/big-avatar-28.jpg",
+		birthday: "03.03.1987",
+		start:"09.05.2014"
 	},
 	{
 		id: "2.2",
@@ -150,6 +188,9 @@ var data = [
 		phone: "(264) 684-4373",
 		mail: "mdixon@gmail.com",
 		photo: "../temp/img/big-avatar-29.jpg",
+		dir: "vertical",
+		birthday: "09.11.1986",
+		start:"05.05.2014"
 	},
 	{
 		id: "2.2.1",
@@ -158,6 +199,8 @@ var data = [
 		phone: "(437) 347-3473",
 		mail: "smorris@gmail.com",
 		photo: "../temp/img/big-avatar-30.jpg",
+		birthday: "25.11.1986",
+		start:"07.03.2005"
 	},
 	{
 		id: "2.2.2",
@@ -166,6 +209,8 @@ var data = [
 		phone: "(436) 348-8692",
 		mail: "jmurray@gmail.com",
 		photo: "../temp/img/big-avatar-31.jpg",
+		birthday: "03.02.1979",
+		start:"12.10.2015"
 	},
 	{
 		id: "2.2.3",
@@ -174,6 +219,8 @@ var data = [
 		phone: "(632) 324-3262",
 		mail: "mbarker@gmail.com",
 		photo: "../temp/img/big-avatar-32.jpg",
+		birthday: "02.12.1954",
+		start:"01.03.2005"
 	},
 	{
 		id: "3",
@@ -182,6 +229,9 @@ var data = [
 		phone: "(451) 251-2578",
 		mail: "esharp@gmail.com",
 		photo: "../temp/img/big-avatar-6.jpg",
+		dir: "vertical",
+		birthday: "02.02.1960",
+		start:"09.01.2000"
 	},
 	{
 		id: "3.1",
@@ -190,6 +240,8 @@ var data = [
 		phone: "(587) 234-8975",
 		mail: "cburke@gmail.com",
 		photo: "../temp/img/big-avatar-7.jpg",
+		birthday: "01.04.1997",
+		start:"01.02.2019"
 	},
 	{
 		id: "3.2",
@@ -198,6 +250,8 @@ var data = [
 		phone: "(875) 231-5332",
 		mail: "esaunders@gmail.com",
 		photo: "../temp/img/big-avatar-8.jpg",
+		birthday: "25.04.1987",
+		start:"09.09.2018"
 	},
 	{
 		id: "3.3",
@@ -206,6 +260,8 @@ var data = [
 		phone: "(361) 423-7234",
 		mail: "smatthews@gmail.com",
 		photo: "../temp/img/big-avatar-9.jpg",
+		birthday: "01.03.1986",
+		start:"01.01.2015"
 	},
 	{
 		id: "3.4",
@@ -214,6 +270,8 @@ var data = [
 		phone: "(565) 525-0672",
 		mail: "kfoster@gmail.com",
 		photo: "../temp/img/big-avatar-10.jpg",
+		birthday: "05.12.1980",
+		start:"06.01.2000"
 	},
 	{
 		id: "4",
@@ -222,6 +280,8 @@ var data = [
 		phone: "(732) 321-2312",
 		mail: "pcox@gmail.com",
 		photo: "../temp/img/big-avatar-11.jpg",
+		birthday: "09.03.1964",
+		start:"03.01.2002"
 	},
 	{
 		id: "4.1",
@@ -230,6 +290,8 @@ var data = [
 		phone: "(743) 235-1263",
 		mail: "ncollins@gmail.com",
 		photo: "../temp/img/big-avatar-12.jpg",
+		birthday: "04.06.1988",
+		start:"09.03.2014"
 	},
 	{
 		id: "4.1.1",
@@ -238,6 +300,8 @@ var data = [
 		phone: "(623) 265-2362",
 		mail: "aday@gmail.com",
 		photo: "../temp/img/big-avatar-33.jpg",
+		birthday: "03.03.1989",
+		start:"03.02.2019"
 	},
 	{
 		id: "4.1.2",
@@ -246,6 +310,8 @@ var data = [
 		phone: "(347) 236-2373",
 		mail: "nnewman@gmail.com",
 		photo: "../temp/img/big-avatar-34.jpg",
+		birthday: "09.10.1980",
+		start:"09.09.2000"
 	},
 	{
 		id: "4.2",
@@ -254,6 +320,8 @@ var data = [
 		phone: "(263) 234-8756",
 		mail: "hblack@gmail.com",
 		photo: "../temp/img/big-avatar-13.jpg",
+		birthday: "12.12.1989",
+		start:"04.07.2016"
 	},
 	{
 		id: "4.3",
@@ -262,5 +330,7 @@ var data = [
 		phone: "(705) 236-5742",
 		mail: "amoore@gmail.com",
 		photo: "../temp/img/big-avatar-5.jpg",
-	},
+		birthday: "02.12.1990",
+		start:"09.03.2015"
+	}
 ];
