af3518bfa16c787e234b472d28b81f7528de8a64
Oleg <sorokin.world@gmail.com>
2020-08-17 15:42:26 +0300

    [dev] simplevault. added sample/backend

--- server/routers/form.js
+++ server/routers/form.js
@@ -1,10 +1,36 @@
-import { invalidRequest } from "../utils/responses";
+import { invalidRequest, notFound } from "../utils/responses";
+import Busboy from "busboy";
+import fs from "fs";
+import path from "path";
 
 export default function (app) {
 	app.post("/form/login", async (req, res) => {
 		if (!req.body) {
-			invalidRequest();
+			invalidRequest(res);
 		}
 		res.status(200).send(JSON.stringify(req.body, null, 2));
+	});
+
+	app.post("/form/upload", (req, res) => {
+		if (req.method === "POST") {
+			const busboy = new Busboy({ headers: req.headers });
+			const response = {};
+
+			busboy.on("file", (fieldname, file, filename) => {
+				const saveTo = path.join(__dirname, "../upload/files/") + filename;
+				response.link = `/form/upload/files/${filename}`;
+
+				file.pipe(fs.createWriteStream(saveTo));
+			});
+
+			busboy.on("finish", () => {
+				res.json(response);
+			});
+
+			return req.pipe(busboy);
+		}
+
+		notFound(res);
+		res.end();
 	});
 }
